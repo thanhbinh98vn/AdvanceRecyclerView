a34259e0431b0c943706ace5018407f8cb2e9788
Binh <thanhbinh19798@gmail.com>
2021-09-22 21:12:50 +0700

    fix add animation

--- app/src/main/java/com/example/demorecyclerviewmutipleviewtype/util/AnimationUtil.kt
+++ app/src/main/java/com/example/demorecyclerviewmutipleviewtype/util/AnimationUtil.kt
@@ -1,18 +1,17 @@
 package com.example.demorecyclerviewmutipleviewtype.util
 
 import android.animation.ValueAnimator
+import android.content.res.Resources
 import android.view.View
 import android.view.animation.DecelerateInterpolator
-
-
 
 object AnimationUtil {
 
     fun expand(clView:View) {
-        val prevHeight: Int = clView.height
+        val height: Int = Resources.getSystem().displayMetrics.heightPixels / 6
         clView.visibility = View.VISIBLE
-        ValueAnimator.ofInt(prevHeight,150).apply {
-            duration = 2000
+        ValueAnimator.ofInt(0, height).apply {
+            duration = 1000
             interpolator = DecelerateInterpolator()
             this.addUpdateListener { animation ->
                 clView.layoutParams.height = animation.animatedValue as Int
@@ -21,5 +20,4 @@ object AnimationUtil {
             start()
         }
     }
-
 }
--- app/src/main/java/com/example/demorecyclerviewmutipleviewtype/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/demorecyclerviewmutipleviewtype/view/activity/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.demorecyclerviewmutipleviewtype.view.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.demorecyclerviewmutipleviewtype.R
 
 class MainActivity : AppCompatActivity() {
